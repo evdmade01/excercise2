f6927515873123c459bfb36228cde46753d908c1
Erik van der Made <erik.van.der.made@cerios.nl>
2015-09-25 21:11:47 +0200

    updated BrowserUtils with GoogleChrome driver (32-bit)
    TODO: fix IEDriver64

--- src/test/java/nl/interaccess/cucumber/ex2/BrowserUtils.java
+++ src/test/java/nl/interaccess/cucumber/ex2/BrowserUtils.java
@@ -1,9 +1,13 @@
 package nl.interaccess.cucumber.ex2;
 
 import java.io.File;
+
+import settings.PrestashopAdminTestSettings;
+
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
@@ -30,16 +34,16 @@ public class BrowserUtils {
 			 * Vervang dit eventueel door een browser naar keuze.
 			*/
 			// FF
-			File pathToBinary = new File("U:/tools/FirefoxPortable/App/Firefox/firefox.exe");
-			FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
-			FirefoxProfile firefoxProfile = new FirefoxProfile();
-//			driver = new FirefoxDriver(ffBinary,firefoxProfile);
+//			File pathToBinary = new File(PrestashopAdminTestSettings.FF_Path);
+//			FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+//			FirefoxProfile firefoxProfile = new FirefoxProfile();
+//			webDriver = new FirefoxDriver(ffBinary,firefoxProfile);
 			
 //			FirefoxProfile profile = new FirefoxProfile();
 //			profile.setPreference("network.proxy.type", 1);
 //			profile.setPreference("network.proxy.http", "srv0220.business.finl.fortis");
 //			profile.setPreference("network.proxy.http_port", 8080);
-			webDriver = new FirefoxDriver(ffBinary,firefoxProfile);
+			
 
 			// IE with proxy settings
 /*			String PROXY = "srv0220.business.finl.fortis:8080";
@@ -54,8 +58,16 @@ public class BrowserUtils {
 			webDriver = new InternetExplorerDriver(cap);
 */
 			// default IE
+//			File pathToBinary = new File(PrestashopAdminTestSettings.IE_Path);
+//			System.setProperty("webdriver.ie.driver", pathToBinary.getAbsolutePath());
 //			webDriver = new InternetExplorerDriver();
-			}
+			
+			// Chrome driver32
+			File pathToBinary = new File(PrestashopAdminTestSettings.Chrome_Path);
+			System.setProperty("webdriver.chrome.driver", pathToBinary.getAbsolutePath());
+			webDriver = new ChromeDriver();
+
+		}
 		
 		/* Stel een timeout in die aangeeft hoe lang de webDriver moet blijven proberen om een element
 		 * dat op de pagina aanwezig zou moeten zijn te selecteren.
